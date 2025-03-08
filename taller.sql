55b28b396b0b79210c9a7017ba61987d9e33234b
toms7ry <tomas.ruizy@upb.edu.co>
2025-03-08 11:38:21 -0500

    Add files via upload

---
+++ taller.sql
@@ -0,0 +1,5 @@
+--CREATE TABLE "productos" ("id"INTEGER,"nombre_producto"TEXT,"precio"REAL,"stock_disponible"NUMERIC,PRIMARY KEY("id" AUTOINCREMENT))
+INSERT INTO "productos"("nombre_producto","precio","stock_disponible")
+--VALUES("tequila",100000,30)
+--VALUES("vodka",90000,40)
+--VALUES("ginebra",180000,20)
